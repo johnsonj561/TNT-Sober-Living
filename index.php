ddb51b7fc04906d179113b77f6b71209836c8ea0
Justin Johnson <johnsonj561@gmail.com>
2017-05-01 20:08:53 -0400

    updated logo and color scheme

--- index.php
+++ index.php
@@ -9,184 +9,119 @@
     <meta name="author" content="TNT Sober Living">
     <title>TNT Sober Living</title>
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/assetts/css/bootstrap.css" rel="stylesheet">
+    <link href="/assetts/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- Custom CSS -->
-    <link href="css/style.css" rel="stylesheet">
-
-
+    <link href="/assetts/css/style.css" rel="stylesheet">
   </head>
 
-  <body>
-    <!-- Top Bar Phone Number -->
-    <div class="top-bar">
-      <span class="pull-right top-bar-call-today"><a href="tel:+6494461709">
-        <span class="glyphicon glyphicon-earphone"></span> CALL TODAY 561-752-1254</a>
-      </span>
+  <?php include_once('/templates/navbar.html'); ?>
 
-      <span class="pull-right top-bar-social-icons">
-        <ul class="list-unstyled list-inline list-social-icons">
-          <li>
-            <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
-          </li>
-          <li>
-            <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
-          </li>
-          <li>
-            <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
-          </li>
-        </ul>
-      </span>
+  <!-- Page Content -->
+  <div class="container content-start">
 
+    <!-- Marketing Icons Section -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">
+          Focusing On What Matters
+        </h1>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Safe</h3>
+          </div>
+          <div class="panel-body">
+            Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Clean</h3>
+          </div>
+          <div class="panel-body">
+            Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Zero Tolerance</h3>
+          </div>
+          <div class="panel-body">
+            Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand tnt-logo" href="/index.html"> 
-            <img src="/assetts/img/logos/tnt-logo-v1-250x100.jpg" alt="TNT Sober Living Logo"> 
-          </a> 
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right pull-right">
-            <li>
-              <a href="/index.html#about">Photos</a>
-            </li>
-            <li>
-              <a href="/assetts/hickory-house-intake.pdf" target="_blank">Print Intake</a>
-            </li>
-            <li>
-              <a href="contact.html">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
+    <!-- /.row -->
+
+    <!-- Mission statement block qoute -->
+    <div class="row">
+      <div class="col-lg-12">
+        <blockquote class="shadow-quote">
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit 
+          amet aliquam vel, ullamcorper sit amet ligula. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+        </blockquote>
       </div>
-      <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
-    <div class="container content-start">
-
-      <!-- Marketing Icons Section -->
-      <div class="row">
-        <div class="col-lg-12">
-          <h1 class="page-header">
-            Focusing On What Matters
-          </h1>
-        </div>
-        <div class="col-md-4">
-          <div class="panel panel-success">
-            <div class="panel-heading">
-              <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Safe</h3>
-            </div>
-            <div class="panel-body">
-              Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="panel panel-success">
-            <div class="panel-heading">
-              <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Clean</h3>
-            </div>
-            <div class="panel-body">
-              Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="panel panel-success">
-            <div class="panel-heading">
-              <h3 class="panel-title"><i class="fa fa-check fa-2x" aria-hidden="true"></i> Zero Tolerance</h3>
-            </div>
-            <div class="panel-body">
-              Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /.row -->
-
-      <!-- Mission statement block qoute -->
-      <div class="row">
-        <div class="col-lg-12">
-          <blockquote class="shadow-quote">
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit 
-            amet aliquam vel, ullamcorper sit amet ligula. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
-          </blockquote>
-        </div>
-      </div>
-
-
-      <!-- Portfolio Section -->
-      <div class="row">
-        <div class="col-lg-12">
-          <h2 class="page-header">Property Photos</h2>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <a href="portfolio-item.html">
-            <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-          </a>
-        </div>
-      </div>
-      <!-- /.row -->
-
     </div>
-    <!-- /.container -->
-
-    <?php require_once('/templates/footer.html'); ?>
 
 
+    <!-- Portfolio Section -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="page-header">Property Photos</h2>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+        </a>
+      </div>
+    </div>
+    <!-- /.row -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+  </div>
+  <!-- /.container -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+  <?php require_once('/templates/footer.html'); ?>
 
-    <!-- Script to Activate the Carousel -->
-    <script>
-      $('.carousel').carousel({
-        interval: 5000 //changes the speed
-      });
-    </script>
+  <!-- jQuery -->
+  <script src="/assetts/js/jquery.js"></script>
+
+  <!-- Bootstrap Core JavaScript -->
+  <script src="/assetts/js/bootstrap.min.js"></script>
 
   </body>
 
